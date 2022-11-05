b546ba901ea9f754070cd845e5473b3bc1778687
ElB5 <eb.brady.ellen@gmail.com>
2022-11-05 16:23:56 -0500

    Part 1 done, test 1 2/2 pass

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,26 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Employer extends AbstractEntity {
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+@Entity
+//add public accessor methods to Employer - add something here???
+public class Employer extends AbstractEntity {
+//
+//    @NotBlank
+//    @Size(max = 50)
+//    private String location;
+//
+//    public String getLocation() {
+//        return location;
+//    }
+//
+//    //not sure need this since only 1 location per employer
+////    public void setLocation(String location) {
+////        this.location = location;
+////    }
+//
+//    public Employer() {};
 
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+//    @Size(max = 500)
+//    private String description;
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDesscription(String desscription) {
+//        this.description = description;
+//    }
+//
+//    public Skill(String description) {};
 }
